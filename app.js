6f586a06a4057e20169fac38aa8911a9e5c5454e
kva <7231950@gmail.com>
2025-02-22 18:26:12 +0300

    change to module

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const axios = require('axios');
+import axios from 'axios';
 
-module.exports = async function(req, res) {
+export default async function(req, res) {
     const formData = JSON.parse(req.payload);
     const tableId = 'mvyihi8x26nven2'; // table contact
+
     const NOCODB_API = process.env.NOCODB_API;
+
     // Подключение к NocoDB и вставка данных
     try {
-        await axios.post('https://app.nocodb.com/api/v2/tables/'+ tableId +'/records', formData, {
+        await axios.post('https://app.nocodb.com/api/v2/tables/' + tableId + '/records', formData, {
             headers: {
                 'xc-token': NOCODB_API
             }
         });
-
         res.json({ success: true, message: 'Данные успешно записаны в NocoDB' });
     } catch (error) {
         console.error('Ошибка при записи в NocoDB:', error);
         res.json({ success: false, message: 'Ошибка при записи данных' });
     }
-};
+}
