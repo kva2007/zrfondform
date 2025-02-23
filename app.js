afa08331b245eadbfe688ced96d0afc4e22f350f
kva <7231950@gmail.com>
2025-02-23 21:14:00 +0300

    CORS

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 
 export default async function({req, res, log, error}) {
+    if (req.method === 'OPTIONS') {
+        return res.send('', 200, {
+            'Access-Control-Allow-Origin': '*.zr-fond.ru',
+            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        })
+    }
+
     const tableId = 'mvyihi8x26nven2';
     const NOCODB_API = process.env.NOCODB_API;
     const formData = req.body;
@@ -14,9 +22,13 @@ export default async function({req, res, log, error}) {
 
        // console.log('NocoDB response:', response.data);
 
-        return res.json({ success: true, message: 'Данные успешно записаны в NocoDB' });
+        return res.json({ success: true, message: 'Данные успешно записаны в NocoDB' },200,{
+            'Access-Control-Allow-Origin': '*.zr-fond.ru',
+        });
     } catch (error) {
         console.error('Ошибка:', error.message);
-        return res.json({ success: false, message: 'Ошибка при обработке или записи данных: ' + error.message });
+        return res.json({ success: false, message: 'Ошибка при обработке или записи данных: ' + error.message },200,{
+            'Access-Control-Allow-Origin': '*.zr-fond.ru',
+        });
     }
 }
